7a6b4007741db62b88cb11be3b20d12b746cb81e
Olof Kindgren <olof.kindgren@gmail.com>
2019-09-26 10:36:54 +0200

    wip: demoapp

--- sw/swervolf_zephyr_demo/src/main.c
+++ sw/swervolf_zephyr_demo/src/main.c
@@ -5,7 +5,109 @@
  */
 
 #include <zephyr.h>
+#include <device.h>
 #include <sys/printk.h>
+#include <disk_access.h>
+#include <fs.h>
+#include <ff.h>
+#include <uart.h>
+
+#define UART_DEVICE_NAME CONFIG_UART_CONSOLE_ON_DEV_NAME
+
+static int lsdir(const char *path);
+
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+
+static const char *disk_mount_pt = "/SD:";
+
+int init_sd(void) {
+
+  /* raw disk i/o */
+  do {
+    static const char *disk_pdrv = "SD";
+    u64_t memory_size_mb;
+    u32_t block_count;
+    u32_t block_size;
+    
+    if (disk_access_init(disk_pdrv) != 0) {
+      printk("Storage init ERROR!");
+      break;
+    }
+    
+    if (disk_access_ioctl(disk_pdrv,
+			  DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+      printk("Unable to get sector count");
+      break;
+    }
+
+    if (disk_access_ioctl(disk_pdrv,
+			  DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+      printk("Unable to get sector size");
+      break;
+    }
+    printk("Sector size %u\n", block_size);
+    
+    memory_size_mb = (u64_t)block_count * block_size;
+    printk("Memory Size(MB) %u\n", (u32_t)memory_size_mb>>20);
+  } while (0);
+  
+  mp.mnt_point = disk_mount_pt;
+  
+  int res = fs_mount(&mp);
+  
+  if (res == FR_OK) {
+    printk("Disk mounted.\n");
+    lsdir(disk_mount_pt);
+  } else {
+    printk("Error mounting disk.\n");
+  }
+  return 0;
+}
+
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk(" [DIR ] %s\n", entry.name);
+		} else {
+			printk(" [FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+
+	return res;
+}
+
+int match_file(char *filename) {
+}  
 
 void main(void)
 {
@@ -40,12 +142,29 @@ void main(void)
   //Exit simulation. No effect on hardware
   sys_write8(1, 0x80001009);
 
+  init_sd();
   printk("Now proceeding to blink the LED\n");
+  printk("Enter a filename to execute it\n");
 
+  struct device *uart_dev = device_get_binding(UART_DEVICE_NAME);
+  char c;
+  char filename[16] = {0};
+  int has_char;
+  int fnidx = 0;
   while (1) {
-    sys_write8(1, 0x80001010);
+    has_char = !uart_poll_in(uart_dev, &c);
+    if (has_char) {
+      //printk("%d\n", c);
+      uart_poll_out(uart_dev, c);
+      if ((c == '\n') || (c == '\r')) {printk("newline\n");
+	break;}
+      filename[fnidx++] = c;
+    }
+    
+    /*    sys_write8(1, 0x80001010);
     k_sleep(1000);
     sys_write8(0, 0x80001010);
-    k_sleep(1000);
+    k_sleep(1000);*/
   }
+  printk("You chose %s\n", filename);
 }
