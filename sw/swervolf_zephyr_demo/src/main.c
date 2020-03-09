bbfc899aaa51f310fd7bcbf2118302d3c67c2980
Olof Kindgren <olof.kindgren@gmail.com>
2020-03-09 22:07:39 +0100

    Update demo app for new GPIO

--- sw/swervolf_zephyr_demo/src/main.c
+++ sw/swervolf_zephyr_demo/src/main.c
@@ -42,10 +42,17 @@ void main(void)
 
   printk("Now proceeding to blink the LED\n");
 
+  u16_t leds = 1;
+  u16_t gpio_old = sys_read16(0x80001012);
+  u16_t gpio_new = sys_read16(0x80001012);
   while (1) {
-    sys_write8(1, 0x80001010);
-    k_sleep(1000);
-    sys_write8(0, 0x80001010);
-    k_sleep(1000);
+    sys_write16(leds, 0x80001010);
+    leds = (leds << 1) | (leds >> 15);
+    gpio_new = sys_read16(0x80001012);
+    if (gpio_old != gpio_new) {
+      printk("GPIO is now %04x\n", gpio_new);
+      gpio_old = gpio_new;
+    }
+    k_sleep(100);
   }
 }
