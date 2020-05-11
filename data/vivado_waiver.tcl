2be4412704b5c15b6bee14dcb4e79d2f83d01b4d
Olof Kindgren <olof.kindgren@gmail.com>
2020-05-11 15:49:45 +0200

    Update vivado_waiver.tcl

--- data/vivado_waiver.tcl
+++ data/vivado_waiver.tcl
@@ -4,10 +4,16 @@ set_msg_config -id "Synth 8-350" -string "litedram_core.v ISERDESE2" -suppress
 set_msg_config -id "Synth 8-350" -string "litedram_core.v IDELAYCTRL" -suppress
 set_msg_config -id "Synth 8-350" -string "litedram_core.v IDELAYE2" -suppress
 set_msg_config -id "Synth 8-350" -string "litedram_core.v PLLE2_ADV" -suppress
+set_msg_config -id "Synth 8-4446" -string "litedram_core.v" -suppress
 set_msg_config -id "Synth 8-6014" -string "litedram_core.v" -suppress
 set_msg_config -id "Synth 8-3936"  -string "memdat_2_reg" -suppress
 set_msg_config -id "Synth 8-3936"  -string "memdat_4_reg" -suppress
 set_msg_config -id "Synth 8-3936"  -string "memdat_18_reg" -suppress
+set_msg_config -id "Synth 8-7023" -string "litedram_core.v IDELAYE2" -suppress
+set_msg_config -id "Synth 8-7023" -string "litedram_core.v IDELAYCTRL" -suppress
+set_msg_config -id "Synth 8-7023" -string "litedram_core.v ISERDESE2" -suppress
+set_msg_config -id "Synth 8-7023" -string "litedram_core.v OSERDESE2" -suppress
+set_msg_config -id "Synth 8-7023" -string "litedram_core.v PLLE2_ADV" -suppress
 set_msg_config -id "DRC REQP-1839" -string "ddr2/ldc/storage" -suppress
 set_msg_config -id "DRC REQP-1840" -string "ddr2/ldc/storage" -suppress
 
@@ -18,5 +24,5 @@ set_msg_config -id "Synth 8-3936" -string "memory_to_writeBack_MUL_HH_reg" -supp
 #SweRV warnings
 set_msg_config -id "DRC REQP-1840" -string "rvtop/mem/Gen_dccm_enable.dccm/mem_bank" -suppress
 
-#AXI CDC
-set_msg_config -id "Synth 8-3332" -string "dc_awchan/buffer/data_reg" -suppress
+#SweRVolf warning
+set_msg_config -id "Synth 8-4446" -string "swervolf_nexys.v STARTUPE2" -suppress
