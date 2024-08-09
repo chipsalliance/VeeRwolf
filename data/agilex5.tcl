c5f05a4a54e7912335b308b81eb3db57f3be6069
Greg Steiert <greg.steiert@intel.com>
2024-08-09 10:02:29 -0700

    updating pins to output on FMC, use HPS_COLD_RESETn

--- data/agilex5.tcl
+++ data/agilex5.tcl
@@ -23,8 +23,8 @@ set_global_assignment -name RTL_SDC_FILE src/veerwolf_0.7.5/data/agilex5.sdc
 set_location_assignment PIN_D8 -to clk -comment IOBANK_6C
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to clk -entity veerwolf_agilex
 
-# Reset
-set_location_assignment PIN_BK118 -to rstn -comment IOBANK_5B
+# Reset PB SW11 HPS_COLD_RESETn Bank 5B 3.3V
+set_location_assignment PIN_BM109 -to rstn -comment IOBANK_5B
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to rstn -entity veerwolf_agilex
 
 # UART RX
@@ -33,9 +33,9 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_uart_rx -entity v
 
 # UART TX
 set_location_assignment PIN_CK4 -to o_uart_tx -comment IOBANK_6B
-set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to o_uart_rx -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to o_uart_tx -entity veerwolf_agilex
 
-# SPI Flash
+# SPI Flash J9 FPGA USER IO 1-4 Bank 6A 3.3V
 set_location_assignment PIN_BU28 -to o_flash_cs_n -comment IOBANK_6A
 set_location_assignment PIN_BP31 -to o_flash_mosi -comment IOBANK_6A
 set_location_assignment PIN_BR28 -to i_flash_miso -comment IOBANK_6A
@@ -46,7 +46,7 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to o_flash_mosi -entit
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_flash_miso -entity veerwolf_agilex
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to o_flash_sclk -entity veerwolf_agilex
 
-# JTAG
+# JTAG J9 FPGA USER IO 5-7 Bank 6 3.3V
 set_location_assignment PIN_BU31 -to i_jtag_tck -comment IOBANK_6A
 set_location_assignment PIN_BM28 -to i_jtag_tms -comment IOBANK_6A
 set_location_assignment PIN_BF21 -to i_jtag_tdi -comment IOBANK_6B
@@ -68,14 +68,16 @@ set_location_assignment PIN_CH12 -to i_sw[4] -comment IOBANK_6A
 set_location_assignment PIN_BU22 -to i_sw[5] -comment IOBANK_6A
 set_location_assignment PIN_BW19 -to i_sw[6] -comment IOBANK_6A
 set_location_assignment PIN_BH28 -to i_sw[7] -comment IOBANK_6A
-set_location_assignment PIN_BE93 -to i_sw[8] -comment IOBANK_2A_T
-set_location_assignment PIN_BE79 -to i_sw[9] -comment IOBANK_2A_T
-set_location_assignment PIN_BF83 -to i_sw[10] -comment IOBANK_2A_T
-set_location_assignment PIN_BE83 -to i_sw[11] -comment IOBANK_2A_T
-set_location_assignment PIN_CD134 -to i_sw[12] -comment IOBANK_5A
-set_location_assignment PIN_CD135 -to i_sw[13] -comment IOBANK_5A
-set_location_assignment PIN_CG134 -to i_sw[14] -comment IOBANK_5A
-set_location_assignment PIN_CH132 -to i_sw[15] -comment IOBANK_5A
+# MAX SPARE Bank 5A 3.3V
+set_location_assignment PIN_CD134 -to i_sw[8] -comment IOBANK_5A
+set_location_assignment PIN_CD135 -to i_sw[9] -comment IOBANK_5A
+set_location_assignment PIN_CG134 -to i_sw[10] -comment IOBANK_5A
+set_location_assignment PIN_CH132 -to i_sw[11] -comment IOBANK_5A
+# HSIO SW24 Bank 2AT 1.1V
+set_location_assignment PIN_BE93 -to i_sw[12] -comment IOBANK_2A_T
+set_location_assignment PIN_BE79 -to i_sw[13] -comment IOBANK_2A_T
+set_location_assignment PIN_BF83 -to i_sw[14] -comment IOBANK_2A_T
+set_location_assignment PIN_BE83 -to i_sw[15] -comment IOBANK_2A_T
 
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[0] -entity veerwolf_agilex
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[1] -entity veerwolf_agilex
@@ -85,14 +87,14 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[4] -entity vee
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[5] -entity veerwolf_agilex
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[6] -entity veerwolf_agilex
 set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[7] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[8] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[9] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[10] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[11] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[12] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[13] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[14] -entity veerwolf_agilex
-set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[15] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[8] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[9] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[10] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "3.3-V LVCMOS" -to i_sw[11] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[12] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[13] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[14] -entity veerwolf_agilex
+set_instance_assignment -name IO_STANDARD "1.1-V" -to i_sw[15] -entity veerwolf_agilex
 
 
 # GPIO (outputs)
@@ -101,19 +103,19 @@ set_location_assignment PIN_BM59 -to o_led[0] -comment IOBANK_2A_T
 set_location_assignment PIN_BH59 -to o_led[1] -comment IOBANK_2A_T
 set_location_assignment PIN_BH62 -to o_led[2] -comment IOBANK_2A_T
 set_location_assignment PIN_BK59 -to o_led[3] -comment IOBANK_2A_T
-# Bank 3BB 1.2V
-set_location_assignment PIN_D74 -to o_led[4] -comment IOBANK_3B_B
-set_location_assignment PIN_F74 -to o_led[5] -comment IOBANK_3B_B
-set_location_assignment PIN_F65 -to o_led[6] -comment IOBANK_3B_B
-set_location_assignment PIN_D65 -to o_led[7] -comment IOBANK_3B_B
-set_location_assignment PIN_K77 -to o_led[8] -comment IOBANK_3B_B
-set_location_assignment PIN_M77 -to o_led[9] -comment IOBANK_3B_B
-set_location_assignment PIN_F77 -to o_led[10] -comment IOBANK_3B_B
-set_location_assignment PIN_H77 -to o_led[11] -comment IOBANK_3B_B
-set_location_assignment PIN_M67 -to o_led[12] -comment IOBANK_3B_B
-set_location_assignment PIN_K67 -to o_led[13] -comment IOBANK_3B_B
-set_location_assignment PIN_H67 -to o_led[14] -comment IOBANK_3B_B
-set_location_assignment PIN_F67 -to o_led[15] -comment IOBANK_3B_B
+# FMC LA Bank 3BT 1.2V
+set_location_assignment PIN_B42 -to o_led[4] -comment IOBANK_3B_B
+set_location_assignment PIN_A45 -to o_led[5] -comment IOBANK_3B_B
+set_location_assignment PIN_A48 -to o_led[6] -comment IOBANK_3B_B
+set_location_assignment PIN_B45 -to o_led[7] -comment IOBANK_3B_B
+set_location_assignment PIN_A51 -to o_led[8] -comment IOBANK_3B_B
+set_location_assignment PIN_B51 -to o_led[9] -comment IOBANK_3B_B
+set_location_assignment PIN_B54 -to o_led[10] -comment IOBANK_3B_B
+set_location_assignment PIN_A54 -to o_led[11] -comment IOBANK_3B_B
+set_location_assignment PIN_B60 -to o_led[12] -comment IOBANK_3B_B
+set_location_assignment PIN_A63 -to o_led[13] -comment IOBANK_3B_B
+set_location_assignment PIN_A60 -to o_led[14] -comment IOBANK_3B_B
+set_location_assignment PIN_B56 -to o_led[15] -comment IOBANK_3B_B
 
 set_instance_assignment -name IO_STANDARD "1.1-V" -to o_led[0] -entity veerwolf_agilex
 set_instance_assignment -name IO_STANDARD "1.1-V" -to o_led[1] -entity veerwolf_agilex
